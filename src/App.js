566487aebd5d5d84a872ca4485164611744a49ed
Jackson Kujur <95626883+00jackson@users.noreply.github.com>
2023-09-21 12:25:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ function App() {
   const [csvData, setCsvData] = useState([]);
 
   useEffect(() => {
-    // Replace 'data.csv' with the path to your CSV file.
     fetch('/demo/dashboard/src/data.csv')
       .then((response) => response.text())
       .then((csv) => {
         Papa.parse(csv, {
-          header: true, // Use the first row as headers
-          dynamicTyping: true, // Convert numeric values to numbers
+          header: true, 
+          dynamicTyping: true, 
           complete: (result) => {
             setCsvData(result.data);
           },
@@ -27,8 +26,6 @@ function App() {
       {csvData.map((row, index) => (
         <div key={index}>
           <span>{row.column1}</span>
-          <span>{row.column2}</span>
-          {/* Add more fields as needed */}
         </div>
       ))}
     </div>
